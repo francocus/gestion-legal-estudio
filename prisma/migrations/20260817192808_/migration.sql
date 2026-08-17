e0de67efb545a9491c15680af8e85b5a193ced16
francocus <cusciannafranco@gmail.com>
2026-08-17 17:18:39 -0300

    Registra migracion que elimina ingestionMode de LegalSource
    La columna ingestionMode se quito del schema en un commit previo pero nunca
    se genero la migracion correspondiente. Esta migracion ya fue aplicada a la
    base de datos de produccion; se registra ahora para que el historial de
    migraciones quede consistente con el schema y cualquier deploy futuro pueda
    reproducirla.

---
+++ prisma/migrations/20260817192808_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ingestionMode` on the `LegalSource` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LegalSource" DROP COLUMN "ingestionMode";
